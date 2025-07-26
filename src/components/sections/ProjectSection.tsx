11ef3bc4d286b82f5250e3a9cc4459869a590c96
Brrendenn <nbr4nd0n2005@gmail.com>
2025-07-26 14:40:54 +0700

    Fixed image positioning

--- src/components/sections/ProjectSection.tsx
+++ src/components/sections/ProjectSection.tsx
@@ -79,7 +79,7 @@ const ProjectSection = () => {
                    <img
                     src={project.imageUrl}
                     alt={project.title}
-                    className="w-full h-56 object-cover transform transition-transform duration-500 group-hover:scale-110"
+                    className="w-full h-56 object-contain transform transition-transform duration-500 group-hover:scale-110"
                   />
                   <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                      <a href={project.liveLink} target="_blank" rel="noopener noreferrer" className="text-white hover:text-zinc-400 p-2 rounded-full bg-zinc-700 bg-opacity-50 transition-colors">
