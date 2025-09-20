42109493635936760b734c3c0ab6825e7afc3b4d
Brandon <nbr4nd0n2005@gmail.com>
2025-09-20 13:51:52 +0700

    Fixed some unused variables so that i can deploy on vercel

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -50,45 +50,6 @@ const AboutSection = () => {
     </svg>
   );
 
-  const BookOpenIcon = ({ className }: ReactIconProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className={className}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
-      />
-    </svg>
-  );
-
-  const MapPinIcon = ({ className }: ReactIconProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className={className}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-      />
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-      />
-    </svg>
-  );
-
   const InfoCard = ({ icon, title, text }: InfoCardProps) => (
     <div className="flex items-center gap-4 p-4 bg-gray-100 dark:bg-zinc-800 rounded-lg shadow-sm">
       <div className="text-zinc-500 dark:text-zinc-400">{icon}</div>
