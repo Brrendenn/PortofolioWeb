90aa1b8ccd8627e2e17f7fdcb403872b4e456c13
Brandon <nbr4nd0n2005@gmail.com>
2025-09-26 14:08:09 +0700

    Changes darkmode/lightmode and modified some things in the navbar

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -45,21 +45,21 @@ const NavBar = () => {
     <nav className="fixed bottom-4 left-1/2 -translate-x-1/2 flex items-center justify-between gap-x-3 md:gap-x-6 bg-zinc-600 backdrop-blur-sm text-white rounded-3xl px-3 py-2 shadow-lg ring-1 ring-white/10 via-zinc-500">
       <button
         onClick={handleScrollToTop}
-        className="flex items-center justify-center h-10 w-10 bg-white dark:bg-zinc-700 rounded-full transition-transform hover:scale-110 active:scale-95"
+        className="flex items-center justify-center h-10 w-10 bg-white dark:bg-zinc-500 rounded-full transition-transform hover:scale-110 active:scale-95"
         aria-label="Scroll to top"
       >
-        <Computer className="h-6 w-6 text-zinc-800" />
+        <Computer className="h-6 w-6 text-zinc-800 dark:text-white" />
       </button>
 
       <ul className="hidden md:flex items-center gap-x-4">
         {navLinks.map((link) => {
           return (
             <li key={link}>
-              <button className="flex items-center w-20 justify-center bg-zinc-500 px-3 py-2 rounded-4xl transition-all duration-200 hover:scale-105 active:scale-95">
+              <button className="flex items-center w-20 justify-center bg-white dark:bg-zinc-500 px-3 py-2 rounded-4xl transition-all duration-200 hover:scale-105 active:scale-95">
                 <a
                   href={`#${link.toLowerCase()}`}
                   onClick={(e) => handleSmoothScroll((e), link.toLowerCase())}
-                  className="font-bold text-sm text-zinc-300 hover:text-white"
+                  className="font-bold text-sm text-zinc-600 hover:text-zinc-800 dark:text-white dark:hover:text-zinc-300"
                 >
                   {link}
                 </a>
@@ -71,7 +71,7 @@ const NavBar = () => {
 
       <button
         onClick={handleCopyEmail}
-        className="flex items-center justify-center bg-white dark:bg-zinc-700 text-zinc-600 dark:text-white text-sm font-semibold rounded-2xl px-3 py-2 md:text-sm transition-all duration-200 hover:scale-105 active:scale-95"
+        className="flex items-center justify-center bg-white dark:bg-zinc-500 text-zinc-600 dark:text-white text-sm font-semibold rounded-2xl px-3 py-2 md:text-sm transition-all duration-200 hover:scale-105 active:scale-95"
         style={{ minWidth: "180px" }}
       >
         {isCopied ? (
